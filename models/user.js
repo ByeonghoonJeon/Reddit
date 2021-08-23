5a13bbb35d9009290e282bdea7953dc928d75dce
Nathan Jeon <jeon4972@snu.ac.kr>
2021-08-23 19:44:00 -0400

    Practice

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new Schema(
   {
@@ -7,5 +8,27 @@ const userSchema = new Schema(
   },
   { timestamps: true }
 );
+
+// Must use function expressions here! ES6 => functions do not bind this!
+userSchema.pre("save", function (next) {
+  // ENCRYPT PASSWORD
+  const user = this;
+  if (!user.isModified("password")) {
+    return next();
+  }
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(user.password, salt, (_, hash) => {
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+// Need to use function to enable this.password to work.
+userSchema.methods.comparePassword = function (password, done) {
+  bcrypt.compare(password, this.password, (err, isMatch) => {
+    done(err, isMatch);
+  });
+};
 
 module.exports = model("User", userSchema);
