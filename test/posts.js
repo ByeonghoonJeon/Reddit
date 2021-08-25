06c41f4ca4403b896e3958d020e34fa8aadcc5cd
Nathan Jeon <jeon4972@snu.ac.kr>
2021-08-25 17:52:15 -0400

    Fixed posts test

--- test/posts.js
+++ test/posts.js
@@ -1,4 +1,5 @@
 // test/posts.js
+const User = require("../models/user");
 const chai = require("chai");
 const chaiHttp = require("chai-http");
 const { describe, it, after } = require("mocha");
@@ -18,37 +19,37 @@ const newPost = {
   url: "https://www.google.com",
   summary: "post summary",
 };
+// User that we'll use for testing purposes
+const user = {
+  username: "poststest",
+  password: "testposts",
+};
+const agent = chai.request.agent(app);
+
+before(function (done) {
+  agent
+    .post("/sign-up")
+    .set("content-type", "application/x-www-form-urlencoded")
+    .send(user)
+    .then(function (res) {
+      done();
+    })
+    .catch(function (err) {
+      done(err);
+    });
+});
 
 describe("Posts", function () {
-  after(function () {
-    Post.findOneAndDelete(newPost);
-    server.close();
-  });
+  after(function (done) {
+    Post.findOneAndDelete(newPost)
+      .then(function () {
+        agent.close();
 
-  it("Should create with valid attributes at POST /posts/new", function (done) {
-    // Checks how many posts there are now
-    Post.estimatedDocumentCount()
-      .then(function (initialDocCount) {
-        const agent = chai.request.agent(app);
-        agent
-          .post("/posts/new")
-          // This line fakes a form post,
-          // since we're not actually filling out a form
-          .set("content-type", "application/x-www-form-urlencoded")
-          // Make a request to create another
-          .send(newPost)
-          .then(function (res) {
-            Post.estimatedDocumentCount()
-              .then(function (newDocCount) {
-                // Check that the database has status 200
-                res.should.have.status(200);
-                // Check that the database has one more post in it
-                newDocCount.should.equal(initialDocCount + 1);
-                done();
-              })
-              .catch(function (err) {
-                done(err);
-              });
+        User.findOneAndDelete({
+          username: user.username,
+        })
+          .then(function () {
+            done();
           })
           .catch(function (err) {
             done(err);
